22829d870b211667146178000e6c876140a506fc
dongchyeon <kimdonghyun90766@gmail.com>
2025-12-04 20:09:35 +0900

    Chore/#3: 스프링 환경 변수가 적용된 후에 mongoClient 생성

--- approval-request-service/src/main/kotlin/org/dongchyeon/approvalrequestservice/ApprovalRequestServiceApplication.kt
+++ approval-request-service/src/main/kotlin/org/dongchyeon/approvalrequestservice/ApprovalRequestServiceApplication.kt
@@ -1,11 +1,30 @@
 package org.dongchyeon.approvalrequestservice
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ApprovalRequestServiceApplication
+class ApprovalRequestServiceApplication {
+
+    private val log = LoggerFactory.getLogger(ApprovalRequestServiceApplication::class.java)
+
+    @Bean
+    fun mongoClient(@Value("\${spring.data.mongodb.uri}") mongoUri: String): MongoClient {
+        log.info("Configuring MongoClient with URI={}", mongoUri)
+        val settings = MongoClientSettings.builder()
+            .applyConnectionString(ConnectionString(mongoUri))
+            .build()
+        return MongoClients.create(settings)
+    }
+}
 
 fun main(args: Array<String>) {
-    runApplication<ApprovalRequestServiceApplication>(*args)
+	runApplication<ApprovalRequestServiceApplication>(*args)
 }
