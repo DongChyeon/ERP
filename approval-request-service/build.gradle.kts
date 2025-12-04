bdda44238c7b5935425e54c0e6a713b24c238030
dongchyeon <kimdonghyun90766@gmail.com>
2025-12-04 20:46:21 +0900

    Chore/#3: 불필요한 의존성 제거

--- approval-request-service/build.gradle.kts
+++ approval-request-service/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
