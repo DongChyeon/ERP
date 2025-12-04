4e124cc31c1dabd5f95adbd240d7eb15c215c3aa
dongchyeon <kimdonghyun90766@gmail.com>
2025-12-04 17:21:01 +0900

    Chore/#3: MongoDB와 Tomcat 설정을 위한 구성 세팅

--- approval-request-service/build.gradle.kts
+++ approval-request-service/build.gradle.kts
@@ -20,7 +20,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
